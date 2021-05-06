385ef3f90ca68d3a5d05431dcba01cd385ca51b7
Jackson Peak <jacksonpeak1@icloud.com>
2021-05-05 21:16:37 -0400

    setup server

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const logger = require("morgan");
+
+const PORT = 3001;
+
+const app = express();
+
+app.use(logger("dev"));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static("public"));
+
+mongoose.connect("mongodb://localhost/workout", {
+  useFindAndModify: false,
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+// app.use(require("./controller/api"));
+// app.use(require("./controller/views"));
+
+app.listen(PORT, () => {
+  console.log(`app running on port ${PORT}`);
+});
