81f7a8b6107854b3eb8a295223a3c603453b1997
Jackson Peak <jacksonpeak1@icloud.com>
2021-05-07 14:38:19 -0400

    pushing process environment PORRT

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const logger = require("morgan");
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 
 const app = express();
 
